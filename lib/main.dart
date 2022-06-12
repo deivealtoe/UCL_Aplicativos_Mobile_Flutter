3daf7ed2e31f61504946beda64bf37fff6d415d5
Deivisson Altoe <deivealtoe@gmail.com>
2022-06-11 21:31:49 -0300

    Tela de cadastro criado por Carlos

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,224 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
-void main() {
-  runApp(const MyApp());
+//var String text;
+
+// - MÁSCARAS
+///#CNPJ
+var maskFormatterCNPJ = new MaskTextInputFormatter(
+    mask: '##.###.###/####-##',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _HomePageState extends State<HomePage> {
+  final controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forms'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(children: []),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+void main() {
+  runApp(MaterialApp(
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+      //color: Color.fromARGB(255, 79, 79, 79),
+      //theme: new ThemeData(
+      //scaffoldBackgroundColor: Color.fromARGB(255, 79, 79, 79)),
+      home: Scaffold(
+    //backgroundColor: Color.fromARGB(255, 79, 79, 79),
+    // GRAY COLOR ARGB = (255, 79, 79, 79)
+    appBar: AppBar(
+      title: Image.asset(
+        'imagem/salonmanager.png',
+        fit: BoxFit.cover,
+        height: 100,
+      ),
+      toolbarHeight: 150,
+      centerTitle: true,
+      //backgroundColor: Color.fromARGB(255, 64, 64, 64),
+      backgroundColor: Color.fromARGB(255, 79, 79, 79),
+    ),
+    body: Form(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(children: [
+            //Dados da empresa
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'CNPJ',
+                icon: Icons.person,
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Razão Social',
+              ),
+            ),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+            TypeSizedBox_Space(),
 
-  final String title;
+            //Endereço
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Estado',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Cidade',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Bairro',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Número',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Complemento',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Nome da rua',
+              ),
+            ),
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+            TypeSizedBox_Space(),
+
+            //Login
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'E-mail',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Senha',
+              ),
+            ),
+            TypeSizedBox_Space_Elements(),
+            const SizedBox(
+              height: 40,
+              child: CustomTextField(
+                label: 'Repita a senha',
+              ),
+            ),
+
+            TypeSizedBox_Space(),
+
+            Builder(builder: (context) {
+              return SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      print(Form.of(context));
+                      Form.of(context)?.validate();
+                    },
+                    icon: const Icon(Icons.save),
+                    label: const Text('Cadastrar'),
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color.fromRGBO(254, 136, 183, 1))),
+                // ROSE COLOR RGBO = (254, 136, 183, 1)
+              );
+            }),
+
+            // Botão
+          ]),
+        ),
+      ),
+    ),
+  )));
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final IconData? icon;
+  final MaskTextInputFormatter? mascara;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const CustomTextField(
+      {Key? key, required this.label, this.icon, this.mascara})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return TextFormField(
+      validator: (text) {
+        if (text == null || text.isEmpty) {
+          return 'É necessário preencher todos os campos';
+        }
+      },
+      //inputFormatters: [mask],
+      cursorHeight: 15,
+      decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: icon == null ? null : Icon(icon),
+          border: OutlineInputBorder(
+              borderRadius: const BorderRadius.all(Radius.circular(40.0)),
+              borderSide:
+                  new BorderSide(color: Color.fromRGBO(254, 136, 183, 1))),
+          fillColor: Colors.white,
+          filled: true),
+      style: const TextStyle(color: Colors.black),
+      cursorColor: Colors.black,
+    );
+  }
+}
+
+class TypeSizedBox_Space extends StatelessWidget {
+  final double altura = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: altura,
+    );
+  }
+}
+
+class TypeSizedBox_Space_Elements extends StatelessWidget {
+  final double altura = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: altura,
     );
   }
 }
