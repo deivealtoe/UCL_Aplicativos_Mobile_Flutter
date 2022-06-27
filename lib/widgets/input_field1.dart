922a572e5027c255e06f2ca08a7b0ca9715f4681
carlosrobertoszjr <69554242+carlosrobertoszjr@users.noreply.github.com>
2022-06-26 22:30:39 -0300

    Add files via upload

--- lib/widgets/input_field1.dart
+++ lib/widgets/input_field1.dart
@@ -3,28 +3,22 @@ import 'package:get/get.dart';
 
 import '../ui/theme.dart';
 
-//late bool _variavel = true;
-//late bool _obscuretext = true;
-
 class MyInputField1 extends StatelessWidget {
-  //late Icon _icon;
-  //late Color _color;
-  //late bool _variavel;
   final Icon? icon;
   final bool obscuretext;
   final bool variavel;
   final String hint;
   final TextEditingController? controller;
   final Widget? widget;
-  const MyInputField1(
-      {Key? key,
-      this.icon,
-      this.obscuretext = false,
-      this.variavel = false,
-      required this.hint,
-      this.controller,
-      this.widget})
-      : super(key: key);
+  const MyInputField1({
+    Key? key,
+    this.icon,
+    this.obscuretext = false,
+    this.variavel = false,
+    required this.hint,
+    this.controller,
+    this.widget,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +26,10 @@ class MyInputField1 extends StatelessWidget {
       margin: EdgeInsets.only(top: 5),
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Container(
-          height: 30,
+          height: 40,
           width: 400,
-          margin: EdgeInsets.only(top: 5),
-          padding: EdgeInsets.only(left: 5),
+          margin: EdgeInsets.only(top: 10),
+          padding: EdgeInsets.only(left: 10),
           decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.grey,
@@ -50,33 +44,17 @@ class MyInputField1 extends StatelessWidget {
                   autofocus: false,
                   cursorColor: Colors.black12,
                   controller: controller,
-                  style: subtitleStile,
                   obscureText: obscuretext,
+                  style: subtitleStile,
                   decoration: InputDecoration(
-                    hintText: hint,
-                    hintStyle: subtitleStile,
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: context.theme.backgroundColor, width: 0)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: context.theme.backgroundColor, width: 0)),
-                    /*suffixIcon: GestureDetector(
-                      child: Icon(
-                        variavel == false
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                        color: Colors.white,
-                      ),
-                      onTap: () {
-                        setState() {
-                          // _variavel = !_variavel;
-                        }
-
-                        ;
-                      },
-                    ),*/
-                  ),
+                      hintText: hint,
+                      hintStyle: subtitleStile,
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: context.theme.backgroundColor, width: 0)),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: context.theme.backgroundColor, width: 0))),
                 ),
               ),
               widget == null ? Container() : Container(child: widget)
@@ -86,6 +64,4 @@ class MyInputField1 extends StatelessWidget {
       ]),
     );
   }
-
-  void setState(Null Function() param0) {}
 }
