41abe7626fda83085b467b79ab37abce2fc34a33
carlosrobertoszjr <69554242+carlosrobertoszjr@users.noreply.github.com>
2022-07-03 22:12:14 -0300

    Add files via upload

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -1,47 +1,41 @@
-
 import 'package:dio/dio.dart';
-import 'package:app_pedrapepeltesoura/models/todo_model.dart';
+import 'package:app_salonmanager/models/todo_model.dart';
 import '../utils/user_simple_preference.dart';
 
-class TodoRepository{
-  
-
-  Future <List<TodoModel>>fetchTodos() async{
-    
+class TodoRepository {
+  Future<List<TodoModel>> fetchTodos() async {
     final dio = Dio();
-    var url = 'https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Saloes/Disponiveis/${UserSimplePreferences.getSalaoId()}';
+    var url =
+        'https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Saloes/Disponiveis/${UserSimplePreferences.getSalaoId()}';
 
     final response = await dio.get(url);
     final list = response.data as List;
 
-    return list.map((json)=> TodoModel.fromJson(json)).toList();
-
-  
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
-class SaloesRepository{
-   final dio = Dio();
+
+class SaloesRepository {
+  final dio = Dio();
   final url = 'https://monktechwebapi-asd.azurewebsites.net/api/Saloes';
 
-  Future <List<SaloesModel>>fetchTodos() async{
+  Future<List<SaloesModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
 
-    return list.map((json)=> SaloesModel.fromJson(json)).toList();
+    return list.map((json) => SaloesModel.fromJson(json)).toList();
   }
 }
-class AgendaSalaoRepository{  
 
-  Future <List<TodoModel>>fetchTodos() async{
-    
+class AgendaSalaoRepository {
+  Future<List<TodoModel>> fetchTodos() async {
     final dio = Dio();
-    var url = 'https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Saloes/${UserSimplePreferences.getSalaoId()}';
+    var url =
+        'https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Saloes/${UserSimplePreferences.getSalaoId()}';
 
     final response = await dio.get(url);
     final list = response.data as List;
 
-    return list.map((json)=> TodoModel.fromJson(json)).toList();
-
-  
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
