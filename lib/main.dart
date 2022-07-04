982ac71e81f4bc1830d7193c95c6062244a38857
Deivisson Altoe <deivealtoe@gmail.com>
2022-07-03 23:31:05 -0300

    Atualizado ícone do app, adicionado mensagem ao tentar marcar horário com campos não preenchidos

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate
--- lib/screen_formulario_cliente.dart
+++ lib/screen_formulario_cliente.dart
@@ -1,23 +1,13 @@
 import 'dart:convert';
-
-import 'package:app_salonmanager/screen_inicial.dart';
 import 'package:app_salonmanager/utils/user_simple_preference.dart';
-import 'package:flutter/foundation.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-
-import 'screen_agenda.dart';
 import 'screen_cadastro.dart';
 import 'screen_saloes.dart';
 import 'ui/theme.dart';
-import 'widgets/Button.dart';
 import 'widgets/input_field1.dart';
-import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'ui/theme.dart';
-
 import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Formulario extends StatefulWidget {
   const Formulario({Key? key}) : super(key: key);
@@ -27,89 +17,79 @@ class Formulario extends StatefulWidget {
 }
 
 class _FormularioState extends State<Formulario> {
-  TextEditingController nom = new TextEditingController();
-  TextEditingController tel = new TextEditingController();
+  TextEditingController nom = TextEditingController();
+  TextEditingController tel = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _appBar(),
-        body: Form(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Preencha suas informações:',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 17, fontFamily: 'Roboto', color: Colors.black),
-                ),
-                MyInputField1(
-                  hint: "Nome",
-                  controller: nom,
-                ),
-                MyInputField1(
-                  hint: "Telefone",
-                  controller: tel,
-                ),
-                TypeSizedBox_Space_Elements(),
-                ElevatedButton.icon(
-                  onPressed: () async {
-                    print("NOM: $nom.text");
-                    print("TEL: $tel.text");
+      appBar: _appBar(),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Preencha suas informações:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 17, fontFamily: 'Roboto', color: Colors.black),
+              ),
+              MyInputField1(
+                hint: "Nome",
+                controller: nom,
+              ),
+              MyInputField1(
+                hint: "Telefone",
+                controller: tel,
+              ),
+              TypeSizedBox_Space_Elements(),
+              ElevatedButton.icon(
+                onPressed: () async {
+                  print("NOM: $nom.text");
+                  print("TEL: $tel.text");
 
-                    print("n: $nom");
+                  print("n: $nom");
 
-                    if (nom.text.isEmpty || tel.text.isEmpty) {
-                      //Form.of(context)?.validate();
-
-                      print("1234566");
-                      //print(Form.of(context));
-                    } else {
-                      var response = await http.put(
-                        Uri.parse(
-                            "https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Agendar/${UserSimplePreferences.getAgendaId()}"),
-                        headers: <String, String>{
-                          'Content-Type': 'application/json',
-                        },
-                        body: jsonEncode(<String, dynamic>{
+                  if (nom.text.isEmpty || tel.text.isEmpty) {
+                    Fluttertoast.showToast(msg: "Ambos os campos são obrigatórios.");
+                  } else {
+                    var response = await http.put(
+                      Uri.parse(
+                          "https://monktechwebapi-asd.azurewebsites.net/api/Agendas/Agendar/${UserSimplePreferences.getAgendaId()}"),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json',
+                      },
+                      body: jsonEncode(
+                        <String, dynamic>{
                           'nomeDoCliente': nom.text,
                           'telefoneDoCliente': tel.text
-                        }),
+                        },
+                      ),
+                    );
+
+                    print(UserSimplePreferences.getAgendaId());
+                    print("Staus code:: ${response.statusCode}");
+
+                    if (response.statusCode == 200) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => const ScreenSaloes()),
                       );
-
-                      print(UserSimplePreferences.getAgendaId());
-                      print("Staus code:: ${response.statusCode}");
-
-                      if (response.statusCode == 200) {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => ScreenSaloes()),
-                        );
-                      }
-
-                      print(response.statusCode);
                     }
-                  },
-                  icon: Icon(Icons.login),
-                  label: Text('Enviar'),
-                  style: ElevatedButton.styleFrom(primary: principalClr),
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
 
-  _erro() {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            Formulario();
-          },
-          child: Text("erro")),
+                    print(response.statusCode);
+                  }
+                },
+                icon: const Icon(Icons.login),
+                label: const Text('Enviar'),
+                style: ElevatedButton.styleFrom(primary: principalClr),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -138,22 +118,3 @@ class _FormularioState extends State<Formulario> {
     );
   }
 }
-/*_appButtonEnviar() {
-  return Builder(builder: (context) {
-    return Container(
-      width: double.infinity,
-      child: ElevatedButton.icon(
-          onPressed: () async {
-            print("Clicou em Cadastrar!");
-            print("Nome: " + nom.text);
-            print("Telefone: " + tel.text);
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => Salao_cliente()),
-            );
-          },
-          icon: Icon(Icons.login),
-          label: Text('Enviar'),
-          style: ElevatedButton.styleFrom(primary: principalClr)),
-    );
-  });
-}*/
